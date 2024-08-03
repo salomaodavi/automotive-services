172611c86e8ef030291ff9754a2e95f342a7e3c6
Salomão Davi <salomaodavicardoso@gmail.com>
2024-08-03 17:16:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+TweenMax.from(".title1", 0.7, {
+    opacity: 0,
+    x: -80, ease: Expo.easeInOut
+});
+
+TweenMax.from(".title2", 0.7, {
+    deplay: 0.2,
+    opacity: 0,
+    x: -80, ease: Expo.easeInOut
+});
+
+TweenMax.from(".carmove", 1.5, {
+    deplay: 1.6,
+    opacity: 0,
+    x: "-100%", ease: Expo.easeInOut
+});
+
+TweenMax.from(".box1", 1.5, {
+    deplay: 1,
+    rotation: 90,
+    opacity: 0,
+    x: -100, ease: Expo.easeInOut
+});
+
+TweenMax.from(".box2", 1.5, {
+    deplay: 1.2,
+    rotation: 180,
+    opacity: 0,
+    x: -400, ease: Expo.easeInOut
+});
+
+TweenMax.from(".box3", 1.5, {
+    deplay: 1,
+    rotation: 190,
+    opacity: 0,
+    x: -600, ease: Expo.easeInOut
+});
+
+TweenMax.from(".logo", 2, {
+    deplay: 0.2,
+    y: 50,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".pattern", 2, {
+    deplay: 0.2,
+    width: 0,
+    opacity: 0,
+    ease: Elastic.easeInOut
+});
+
+
+TweenMax.staggerFrom(".menu ul li", 0.8, {
+    deplay: 0.2,
+    y: 50,
+    opacity: 0,
+    scale: 2,
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.from(".user", 2, {
+    deplay: 0.2,
+    y: 50,
+    opacity: 0,
+    roration: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(".media ul li", 2, {
+    deplay: 0.2,
+    opacity: 0,
+    scale: 0,
+    ease: Expo.easeInOut
+}, 0.2);
+
+TweenMax.from(".content", 2, {
+    deplay: 0.2,
+    y: 50,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".box3", 7, {
+    deplay: 2.6,
+    rotation: 360,
+    repeat: -1, 
+    ease: Power0.easeNone
+});
+
+
